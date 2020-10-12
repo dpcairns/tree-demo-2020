a479b7fbb4e18dc149e3025ea71cf17ca821262c
harriet-the-spy <dpcairns@gmail.com>
2020-10-12 11:39:58 -0700

    add working radio buttons with correct tree name

--- app.js
+++ app.js
@@ -5,9 +5,37 @@ const correctName = document.querySelector('h2');
 const rightOrWrong = document.querySelector('#right-or-wrong');
 const nextButton = document.querySelector('button');
 const nextDiv = document.querySelector('next');
-const radios = document.querySelectorAll('input');
 
 // initialize state
 
 const remainingTrees = rawTreeData.slice();
 
+function getRandomTree(someArray) {
+    const index = Math.floor(Math.random() * someArray.length);
+
+    return someArray[index];
+}
+
+// go grab a cononical correct tree
+const correctTree = getRandomTree(remainingTrees);
+// go grab an incorrect tree from the rawTrees
+let incorrectTree = getRandomTree(rawTreeData);
+
+// now, in the event that the two trees are the same
+while (correctTree.id === incorrectTree.id) {
+    // go grab another incorrectTree UNTIL they are not the same
+    incorrectTree = getRandomTree(rawTreeData);
+}
+
+const radios = document.querySelectorAll('input');
+const images = document.querySelectorAll('label > img');
+
+
+correctName.textContent = correctTree.name;
+radios[0].value = correctTree.id;
+images[0].src = correctTree.image;
+
+radios[1].value = incorrectTree.id;
+images[1].src = incorrectTree.image;
+
+// set event listeners to update state and DOM
