a6f58871696c1d42c36c7b38cae526ee6c76a0b1
harriet-the-spy <dpcairns@gmail.com>
2020-10-12 13:33:52 -0700

    fix removeById bug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const correctName = document.querySelector('h2');
 const rightOrWrong = document.querySelector('#right-or-wrong');
 const nextButton = document.querySelector('button');
 const nextDiv = document.querySelector('#next');
+const radios = document.querySelectorAll('input');
+const images = document.querySelectorAll('label > img');
 
 // initialize state
 
@@ -20,63 +22,80 @@ function getRandomTree(someArray) {
 
 function removeById(someId) {
     for (let i = 0; i < remainingTrees.length; i++) {
-        if (someId === remainingTrees[i]) {
+        if (someId === remainingTrees[i].id) {
             remainingTrees.splice(i, 1);
         }    
     }
 }
+
+function setupQuestion() {
+    if (remainingTrees.length === 0) {
+        alert('done!');
+    }
+
+    nextDiv.classList.add('hidden');
+
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = false;
+        radios[i].checked = false;
+        images[i].style.opacity = 1;
+    } 
+
 // go grab a cononical correct tree
-const correctTree = getRandomTree(remainingTrees);
+    const correctTree = getRandomTree(remainingTrees);
 // go grab an incorrect tree from the rawTrees
-let incorrectTree = getRandomTree(rawTreeData);
+    let incorrectTree = getRandomTree(rawTreeData);
 
 // now, in the event that the two trees are the same
-while (correctTree.id === incorrectTree.id) {
+    while (correctTree.id === incorrectTree.id) {
     // go grab another incorrectTree UNTIL they are not the same
-    incorrectTree = getRandomTree(rawTreeData);
-}
-
-const radios = document.querySelectorAll('input');
-const images = document.querySelectorAll('label > img');
+        incorrectTree = getRandomTree(rawTreeData);
+    }
 
 
-correctName.textContent = correctTree.name;
-radios[0].value = correctTree.id;
-images[0].src = correctTree.image;
+    correctName.textContent = correctTree.name;
+    radios[0].value = correctTree.id;
+    images[0].src = correctTree.image;
 
-radios[1].value = incorrectTree.id;
-images[1].src = incorrectTree.image;
+    radios[1].value = incorrectTree.id;
+    images[1].src = incorrectTree.image;
 
 
 
 // set event listeners to update state and DOM
-for (let i = 0; i < radios.length; i++) {
-    radios[i].addEventListener('change', (e) => 
-    {
-        guessesTaken++;
-        nextDiv.classList.toggle('hidden');
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].addEventListener('change', (e) => 
+        {
+            guessesTaken++;
+            nextDiv.classList.remove('hidden');
             
-        for (let i = 0; i < radios.length; i++) {
-            radios[i].disabled = true;
-            images[i].style.opacity = .5;
-        } 
+            for (let i = 0; i < radios.length; i++) {
+                radios[i].disabled = true;
+                images[i].style.opacity = .5;
+            } 
 
-        const theySelectedTheRightTree = e.target.value === correctTree.id;
+            const theySelectedTheRightTree = e.target.value === correctTree.id;
 
-        console.log(theySelectedTheRightTree);
+            console.log(theySelectedTheRightTree);
 
-        if (theySelectedTheRightTree) {
+            if (theySelectedTheRightTree) {
             // remove tree from remainingTrees
-            removeById(e.target.value);
+                removeById(e.target.value);
 
             // display "correct!"
-            rightOrWrong.textContent = 'correct!!';
-        } else {
-            missedTrees.push(e.target.value);
+                rightOrWrong.textContent = 'correct!!';
+            } else {
+                missedTrees.push(e.target.value);
 
             // display "wrong!"
-            rightOrWrong.textContent = 'wrong!!';
-        }
+                rightOrWrong.textContent = 'wrong!!';
+            }
         
-    });
+        });
+    }
 }
+
+
+setupQuestion();
+
+nextButton.addEventListener('click', setupQuestion);
