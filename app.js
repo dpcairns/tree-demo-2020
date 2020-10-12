606d7c11effa85af7b6c8265e54d61932a56c4ca
harriet-the-spy <dpcairns@gmail.com>
2020-10-12 11:55:23 -0700

    add event listener to radio button with e.target.value

--- app.js
+++ app.js
@@ -38,4 +38,20 @@ images[0].src = correctTree.image;
 radios[1].value = incorrectTree.id;
 images[1].src = incorrectTree.image;
 
+
+function handleClick() {
+    nextDiv.classList.toggle('hidden');
+    
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = true;
+        images[i].style.opacity = .5;
+    } 
+}
+
 // set event listeners to update state and DOM
+for (let i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', (e) => {
+        // e is used whenever you need information/access to the thing the user clicked.
+        console.log(e.target.value);
+    });
+}
